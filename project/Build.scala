c508f27ae66a34e97ba50a5f100bfddc393a46a9
James Roper <james@jazzy.id.au>
2014-06-16 01:47:12 +0200

    Released 1.4.0

--- project/Build.scala
+++ project/Build.scala
@@ -16,7 +16,7 @@ object ApplicationBuild extends Build {
       else                                   Some("releases"  at nexus + "maven-releases/")
     },
     organization := "com.typesafe.play.extras",
-    version := "1.4.0-SNAPSHOT",
+    version := "1.4.0",
     scalaVersion := "2.11.1",
     crossScalaVersions := Seq("2.11.1", "2.10.4")
   )
